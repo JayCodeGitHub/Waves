d49a2ad72cc8f2a70caf46fed95b9d23a9522cc7
JayCode Developer <JayCode.contakt@gmail.com>
2023-08-22 17:39:12 +0200

    Setup SEO component

---
+++ src/components/SEO.tsx
@@ -0,0 +1,15 @@
+import { Helmet } from "react-helmet";
+
+const SEOComponent = () => {
+  return (
+    <Helmet>
+      <title>Waves</title>
+      <meta
+        name="description"
+        content="Website offers a music player that allows you to play your favorite songs in high sound quality. The site is fully responsive, which means you can listen to music on any device."
+      />
+    </Helmet>
+  );
+};
+
+export default SEOComponent;
