d25fec6e6100a47b6d0d3abc2393bc538c73142e
JayCode Developer <JayCode.contakt@gmail.com>
2023-08-23 18:30:04 +0200

    Style main page for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import Player from "./components/player";
 function App() {
   const [isLibraryVisible, setIsLibraryVisible] = useState(false);
   return (
-    <div className="w-screen flex justify-center flex-col items-center py-10">
-      <div className="w-1/2 flex justify-between gap-10">
+    <div className="w-screen flex justify-center flex-col items-center lg:py-10 py-5">
+      <div className="lg:w-1/2 w-full px-5 flex justify-between gap-10">
         <h1 className=" text-xl font-bold">Waves</h1>
         <button
           className=" text-xl"
