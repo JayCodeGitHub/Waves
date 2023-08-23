161bf6564d39e65da69e46fb3ad1e808f6559a11
JayCode Developer <JayCode.contakt@gmail.com>
2023-08-23 02:05:59 +0200

    Create player component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Library from "./components/library";
+import Player from "./components/player";
 
 function App() {
   return (
     <div id="Wrapper">
       <h1>Waves</h1>
       <audio src="/audio/1.mp3" controls />
+      <Library />
+      <Player />
     </div>
   );
 }
