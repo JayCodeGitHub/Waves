a4c78d57ea6f6048a87d5f552579aa81972c76b3
JayCode Developer <JayCode.contakt@gmail.com>
2023-08-27 00:15:06 +0200

    Add custom control buttons

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -1,14 +1,97 @@
+import { useState, useRef, useEffect } from "react";
 import { Songs } from "../assets/songs";
 import { usePlayer } from "../hooks/usePlayer";
 
 const Player = () => {
   const { song } = usePlayer();
+
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [duration, setDuration] = useState<number>(0);
+  const [currnetTime, setCurrentTime] = useState<number>(0);
+
+  const audioPlayer = useRef<HTMLAudioElement | null>(null);
+  const progressBar = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    if (audioPlayer.current) {
+      audioPlayer.current.onloadedmetadata = () => {
+        const seconds = Math.floor(audioPlayer.current?.duration || 0);
+        setDuration(seconds);
+        if (progressBar.current != null) {
+          progressBar.current!.max = seconds.toString();
+        }
+      };
+    }
+  }, [song]);
+
+  const calculateTime = (secs: number) => {
+    const minutes = Math.floor(secs / 60);
+    const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const seconds = Math.floor(secs % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${returnedMinutes}:${returnedSeconds}`;
+  };
+
+  const togglePlayPause = () => {
+    const prevValue = isPlaying;
+    setIsPlaying(!prevValue);
+    if (audioPlayer.current) {
+      if (!prevValue) {
+        audioPlayer.current.play();
+      } else {
+        audioPlayer.current.pause();
+      }
+    }
+  };
+
+  const handleTimeUpdate = () => {
+    if (audioPlayer.current) {
+      setCurrentTime(audioPlayer.current.currentTime);
+      progressBar.current!.value = audioPlayer.current.currentTime.toString();
+    }
+  };
+
+  const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+    if (audioPlayer.current) {
+      audioPlayer.current!.currentTime = Number(newValue);
+    }
+  };
+
   return (
-    <div>
-      <audio autoPlay src={Songs[song].song} controls />
-      <h1>{Songs[song].title}</h1>
-      <h2>{Songs[song].author}</h2>
-      <img src={Songs[song].cover} />
+    <div className="flex flex-col justify-center items-center gap-5 py-10">
+      <img src={Songs[song].cover} className=" rounded-full" />
+      <div className="flex flex-col justify-center items-center gap-2">
+        <h2 className="text-2xl font-bold">{Songs[song].title}</h2>
+        <h3 className="text-lg text-gray-500">{Songs[song].author}</h3>
+      </div>
+      <div className="flex flex-col items-center gap-2">
+        <audio
+          ref={audioPlayer}
+          src={Songs[song].song}
+          controls
+          onTimeUpdate={handleTimeUpdate}
+          className="hidden"
+          onEnded={() => setIsPlaying(false)}
+        />
+        <div className="flex gap-2">
+          <div className=" w-12">{calculateTime(currnetTime)}</div>
+          <input
+            ref={progressBar}
+            type="range"
+            defaultValue={0}
+            onChange={handleSliderChange}
+          />
+          <div>{calculateTime(duration)}</div>
+        </div>
+      </div>
+      <div className="flex gap-2">
+        <button>Back</button>
+        <button onClick={togglePlayPause}>
+          {isPlaying ? "Pause" : "Play"}
+        </button>
+        <button>Forward</button>
+      </div>
     </div>
   );
 };
